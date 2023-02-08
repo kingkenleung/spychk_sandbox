873010adfc680b2650e7d25b01235e45d16d6961
kingkenleung <30319344+kingkenleung@users.noreply.github.com>
2023-02-08 23:06:09 +0800

    express generated

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const express = require('express')
-const app = express()
+const createError = require('http-errors');
+const express = require('express');
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+const indexRouter = require('./routes/index');
 
-app.listen(3000)
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
